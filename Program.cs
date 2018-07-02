ea365e6de0c27529881f2077127ce35d95b1e3ff
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-02 16:15:23 -0500

    Added if statements for the delimeters

--- Program.cs
+++ Program.cs
@@ -6,57 +6,60 @@ namespace URLEncoder
     {
         static void Main(string[] args)
         {
-         
+
             Console.WriteLine("Enter a sting: ");
             String input = Console.ReadLine();
             String result = "";
-            String [] blocks = new String[] { "{", "}", "|", "^", "[", "]", "`"};
-            String [] queryCharacters = new string[] { ";", "/", "?", ":", "@", "&", "=", "+", "$", ","};
-            String[] delimeter = new string[] { "<", ">", "#", "%", "\"" }; 
+            char[] blocks = new char[] {'{', '}', '|', '^', '[', ']', '`'}; 
+            char [] queryCharacters = new char [] { ';', '/', '?', ':', '@', '&', '=', '+', '$', ',' }; 
+            char[] delimeter = new char[] { '<', '>', '#', '%', '\"'};
+          
 
-           // int count = 0; // counts number of spaces 
+            // int count = 0; // counts number of spaces 
 
             for (int i = 0; i < input.Length; i++)
             {
                 if (Char.IsWhiteSpace(input[i]))
-              
+
                 {
-                    result += "%20"; 
+                    result += "%20";
                     // count += 1; 
+                }
+                else if (input[i] == delimeter[0]) { 
+                   
+                    result += "%3C";
 
                 }
-                else if (true)  {  // if input is control_char
-                    // print invalid 
-                
-                }
-                else if (true) //  input is delimeter 
-                { 
-                
-                    // Convert delimeter to "%3c" 
-                }
-                else if (true) { // input is block 
-                       // convert to url encoded format 
-                
-                
-                }
+                else if (input[i] == delimeter[1])
+                {
 
+                    result += "%3E";
 
-                else if (true) { // input is query_characters 
-                    // convert to url encoded format 
+                }
+                else if (input[i] == delimeter[2])
+                {
+
+                    result += "%23";
+
+                }
+                else if (input[i] == delimeter[3])
+                {
+                    
+                    result += "%25";
+
+                }
+                else if (input[i] == delimeter[4])
+                {
+
+                    result += "%22";
 
                 }
                 else
                 {
                     result += input[i];
-                    // count = 0; 
                 }
-            }
+            } 
             Console.WriteLine(result);
-           //  Console.WriteLine("Number of spaces = {0} ", count);
-
-
-            // Console.WriteLine(input); 
-
         }
     }
 }
