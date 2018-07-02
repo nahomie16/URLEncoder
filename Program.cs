26391ebecc95b1cfe1cea931b9df45996dc83b58
Nahom Assefa <ndagfc@mail.missouri.edu>
2018-07-02 16:35:39 -0500

    Added more blocks and query if statements

--- Program.cs
+++ Program.cs
@@ -25,8 +25,9 @@ namespace URLEncoder
                     result += "%20";
                     // count += 1; 
                 }
-                else if (input[i] == delimeter[0]) { 
-                   
+                else if (input[i] == delimeter[0])
+                {
+
                     result += "%3C";
 
                 }
@@ -44,7 +45,7 @@ namespace URLEncoder
                 }
                 else if (input[i] == delimeter[3])
                 {
-                    
+
                     result += "%25";
 
                 }
@@ -54,10 +55,96 @@ namespace URLEncoder
                     result += "%22";
 
                 }
+                else if (input[i] == blocks[0])
+                {
+
+                    result += "%7B";
+                }
+                else if (input[i] == blocks[1])
+                {
+
+                    result += "%7D";
+                }
+                else if (input[i] == blocks[2])
+                {
+
+                    result += "%7C";
+                }
+                else if (input[i] == blocks[3])
+                {
+
+                    result += "%5E";
+                }
+                else if (input[i] == blocks[4])
+                {
+
+                    result += "%5B";
+                }
+                else if (input[i] == blocks[5])
+                {
+
+                    result += "%5D";
+                }
+                else if (input[i] == blocks[6])
+                {
+
+                    result += "%5C";
+                }
+                else if (input[i] == queryCharacters[0])
+                {
+
+                    result += "%3B";
+                }
+                else if (input[i] == queryCharacters[1])
+                {
+
+                    result += "%2F";
+                }
+                else if (input[i] == queryCharacters[2])
+                {
+
+                    result += "%3F";
+                }
+                else if (input[i] == queryCharacters[3])
+                {
+
+                    result += "%3A";
+                }
+                else if (input[i] == queryCharacters[4])
+                {
+
+                    result += "%40";
+                }
+                else if (input[i] == queryCharacters[5])
+                {
+
+                    result += "%26";
+                }
+                else if (input[i] == queryCharacters[6])
+                {
+
+                    result += "%3D";
+                }
+                else if (input[i] == queryCharacters[7])
+                {
+
+                    result += "%2B";
+                }
+                else if (input[i] == queryCharacters[8])
+                {
+
+                    result += "%24";
+                }
+                else if (input[i] == queryCharacters[9])
+                {
+
+                    result += "%2C";
+                }
                 else
                 {
                     result += input[i];
                 }
+
             } 
             Console.WriteLine(result);
         }
